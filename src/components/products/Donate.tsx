ab1b81146f05f492631a146dde16c852d3cafee8
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-03-16 21:59:25 +0600

    donar info save to database

--- src/components/products/Donate.tsx
+++ src/components/products/Donate.tsx
@@ -7,95 +7,81 @@ type Inputs = {
   title: string;
   category: string;
   size: string;
-  description: string
+  description: string;
 };
 
 const Donate = () => {
+  const [createProducts, { isSuccess }] = useCreateProductsMutation();
 
-    const [createProducts, { isSuccess, }] = useCreateProductsMutation()
-    
-    if(isSuccess){
-      toast.success('Clothes Added')
-    }
-  const {
-    register,
-    handleSubmit,
-    reset,
-   
-  } = useForm<Inputs>();
+  if (isSuccess) {
+    toast.success("Clothes Added");
+  }
+  const { register, handleSubmit, reset } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = async (data) => {
-    
-    
-  
-   
-
-
     await createProducts(data).unwrap();
-
-
-    
 
     reset();
   };
 
-  
-  
- 
-
   return (
     <div className="bg-[#001529]  px-8 py-20 min-h-screen">
-     <h1 className="text-3xl font-bold text-center my-12 text-white">Donate winter clothes</h1>
+      <h1 className="text-3xl font-bold text-center my-12 text-white">
+        Donate winter clothes
+      </h1>
       <form onSubmit={handleSubmit(onSubmit)}>
-       <div className="lg:flex justify-evenly items-center gap-4 mb-8">
-       
-       
-       <div className="w-full">
-        <h2 className="text-white font-semibold my-3">Title</h2>
-       <input
-       placeholder="Enter Title"
-       className="h-8 rounded-md w-full px-3"
-       {...register("title" )} />
-       </div>
-       <div className="w-full">
-        <h2 className="text-white font-semibold my-3">Image</h2>
-       <input
-       placeholder="Photo url"
-       className="h-8 rounded-md w-full px-3"
-       {...register("image" )} />
-       </div>
+        <div className="lg:flex justify-evenly items-center gap-4 mb-8">
+          <div className="w-full">
+            <h2 className="text-white font-semibold my-3">Title</h2>
+            <input
+              placeholder="Enter Title"
+              className="h-8 rounded-md w-full px-3"
+              {...register("title")}
+            />
+          </div>
+          <div className="w-full">
+            <h2 className="text-white font-semibold my-3">Image</h2>
+            <input
+              placeholder="Photo url"
+              className="h-8 rounded-md w-full px-3"
+              {...register("image")}
+            />
+          </div>
         </div>
         <div className="lg:flex justify-evenly items-center gap-4 ">
-        <div className="w-full">
-        <h2 className="text-white font-semibold my-3">Category</h2>
-       <input
-       placeholder="Enter Category"
-       className="h-8 rounded-md w-full px-3"
-       {...register("category" )} />
-       </div>
-       <div className="w-full">
-        <h2 className="text-white font-semibold my-3">Size</h2>
-       <input
-       placeholder="Enter Title"
-       className="h-8 rounded-md w-full px-3"
-       {...register("size" )} />
-       </div>
+          <div className="w-full">
+            <h2 className="text-white font-semibold my-3">Category</h2>
+            <input
+              placeholder="Enter Category"
+              className="h-8 rounded-md w-full px-3"
+              {...register("category")}
+            />
+          </div>
+          <div className="w-full">
+            <h2 className="text-white font-semibold my-3">Size</h2>
+            <input
+              placeholder="Enter Title"
+              className="h-8 rounded-md w-full px-3"
+              {...register("size")}
+            />
+          </div>
         </div>
 
         <div className="w-full">
-        <h2 className="text-white font-semibold my-3">Description</h2>
-       <input
-       placeholder="Write description"
-       className="h-16 rounded-md w-full px-3"
-       {...register("description" )} />
-       </div>
-        
+          <h2 className="text-white font-semibold my-3">Description</h2>
+          <input
+            placeholder="Write description"
+            className="h-16 rounded-md w-full px-3"
+            {...register("description")}
+          />
+        </div>
+
         <div className="text-center my-12 font-bold">
-        <input 
-        className="py-4 px-8 bg-white text-blue-700  rounded-md"
-        type="submit" />
+          <input
+            className="py-4 px-8 bg-white text-blue-700  rounded-md"
+            type="submit"
+          />
         </div>
       </form>
-      
     </div>
   );
 };
--- src/components/products/ShowDetails.tsx
+++ src/components/products/ShowDetails.tsx
@@ -1,8 +1,9 @@
 import { useParams } from "react-router-dom";
-import { useGetProductsQuery } from "../../pages/redux/features/product/prodductApi";
+import { useCreateProductsMutation, useGetProductsQuery } from "../../pages/redux/features/product/prodductApi";
 
 import { DotWave } from "@uiball/loaders";
 import { toast } from "sonner";
+import { useState } from "react";
 
 type TParameter = {
   _id: string;
@@ -11,16 +12,31 @@ type TParameter = {
 const ShowDetails = () => {
   const { data, isLoading } = useGetProductsQuery("");
   const detailsData = useParams();
+  const [amount, setAmount] = useState<number | string>(""); // State to hold the donated amount
+  const [donarInfoSave, {isSuccess}] = useCreateProductsMutation()
+
+
+  // Method to handle donation submission
+  const handleDonation = async () => {
+    
+
+    try {
+
+  await  donarInfoSave(amount).unwrap()
+
+  if (isSuccess) {
+    toast.success("Thank you for the donation");
+  }             
+      
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
  
 
-  const handleClose = ()=>{
-    toast.success('Thank you! Donation Done')
-  }
-
-
-
   const singleData = data?.find((d: TParameter) => d._id == detailsData._id);
-  console.log(singleData);
+
   return (
     <div>
       {isLoading ? (
@@ -42,12 +58,18 @@ const ShowDetails = () => {
             <p>Description: {singleData.description}</p>
             <div className="card-actions justify-end">
               <button
-               onClick={() => (document.getElementById("my_modal_5") as HTMLDialogElement)?.showModal()}
-              className="btn btn-primary">Donate Now</button>
+                onClick={() =>
+                  (
+                    document.getElementById("my_modal_5") as HTMLDialogElement
+                  )?.showModal()
+                }
+                className="btn btn-primary"
+              >
+                Donate Now
+              </button>
             </div>
             {/* modal start */}
-        
-           
+
             <dialog
               id="my_modal_5"
               className="modal modal-bottom sm:modal-middle"
@@ -59,14 +81,19 @@ const ShowDetails = () => {
                 </p>
                 <div>
                   <h1>Amount: </h1>
-                  <input type="number" placeholder="$0000" className="input input-bordered input-accent w-full max-w-xs" />
-                 
-                  </div>
+                  <input
+                    type="number"
+                    placeholder="$0000"
+                    className="input input-bordered input-accent w-full max-w-xs"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                  />
+                </div>
                 <div className="modal-action">
-                <form method="dialog">
-                <button
-                onClick={handleClose}
-                className="btn btn-success">Submit</button>
+                  <form method="dialog">
+                    <button onClick={handleDonation} className="btn btn-success">
+                      Submit
+                    </button>
                   </form>
                 </div>
               </div>
